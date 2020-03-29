ac8aa56793adb3723e516124b873533bee0f3ba9
Upinder <upindersangha01@gmail.com>
2020-03-29 23:31:49 +0530

    Add gitignore and host website on heroku

--- app.js
+++ app.js
@@ -130,6 +130,9 @@ app.post("/delete", function (req, res) {
 });
 
 
-app.listen(3000, function () {
-	console.log("server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "")
+	port = 3000;
+app.listen(port, function () {
+	console.log("server started");
 });
