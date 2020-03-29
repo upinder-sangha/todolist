82fa86adb9059fd9420e18f4d6bb2152bbf72b0c
Upinder <upindersangha01@gmail.com>
2020-03-29 23:13:08 +0530

    use mongodb atlas

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://admin-upinder:Test123@cluster0-jay09.mongodb.net/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const itemSchema = new mongoose.Schema({
 	name: String
